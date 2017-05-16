2aa7132aecd4c1a04d1a915ddcb0694c69cf61b1
chaitukanumolu <chaitu.kanumolu@gmail.com>
2017-05-16 07:23:53 +0530

    Add files via upload

---
+++ DataAnalysis/pandapractice.py
@@ -0,0 +1,11 @@
+from pandas import DataFrame, Series
+import pandas as pd
+import json
+
+path = '/Users/ckanumolu/Desktop/MachineLearning/DataForGitHub/usagov_bitly_data2013-05-17-1368832207'
+records=[json.loads(line) for line in open(path)]
+#print(records[0])
+
+
+frame = DataFrame(records)
+print(frame)
